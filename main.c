d36ff476ae906d48e92f0a42d52b12bb0140a985
theOnyangos <otienodennis29@gmail.com>
2022-11-15 01:43:08 -0800

    Setting up the main file and the header file

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+/**
+* main - carries out the read, execute then print output loop
+* @ac: argument count
+* @av: argument vector
+* @envp: environment vector
+*
+* Return: 0
+*/
+
+int main(int ac, char **av, char *envp[])
+{}
